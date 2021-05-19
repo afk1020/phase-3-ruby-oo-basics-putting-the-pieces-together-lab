35453331d60f15a5bef99e4b1730e9d63c5767e7
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-05-19 12:29:50 -0500

    commit tree 302b2dec2b7b45b9b8adf38c4086ec1c61c2a459

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,35 @@
+require 'pry'
+class Book
+    def initialize(title)
+       # binding.pry
+        @title = title
+    end
+    def title
+        @title
+    end
+    def author
+        @author
+    end
+    def author=(author_name)
+        @author = author_name
+    end
+    def page_count
+        @page_count
+    end
+    def page_count=(page)
+        @page_count=page
+    end
+    def genre
+        @genre
+    end
+    def genre=(genre_type)
+        @genre=genre_type
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+   
+end
+
 
 
